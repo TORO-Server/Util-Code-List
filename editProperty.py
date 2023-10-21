eb207054a6119e190fe6eef2e7190373703e423f
malken21 <Malken_Marumasa@outlook.jp>
2023-10-21 18:01:40 +0900

    Create editProperty.py

---
+++ editProperty.py
@@ -0,0 +1,25 @@
+import re
+import sys
+
+# server.properties のファイルパス
+PATH = "server.properties"
+# 変更したいプロパティの名前
+KEY = sys.argv[1:]
+# プロパティに設定したい値
+VALUE = sys.argv[2:]
+
+
+def edit_server_properties(file_path, property_key, new_value):
+    # ファイルを開く
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+    # プロパティを探す
+    for i, line in enumerate(lines):
+        if line.startswith(property_key + "="):
+            lines[i] = f"{property_key}={re.escape(new_value)}\n"
+    # 変更を書き戻す
+    with open(file_path, 'w') as file:
+        file.writelines(lines)
+
+
+edit_server_properties(PATH, KEY, VALUE)
