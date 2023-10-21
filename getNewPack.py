b2135951a2eb7197e8d795753aa40eaffdb02ad8
malken21 <Malken_Marumasa@outlook.jp>
2023-10-21 17:28:58 +0900

    Update getNewPack.py

--- getNewPack.py
+++ getNewPack.py
@@ -1,12 +1,14 @@
 from urllib import request
 import json
 
+# -----設定項目----- start
 User = "TORO-Server"
 Repository = "TORO-ResourcePack"
+# -----設定項目----- end
+
 
 # URL
 URL = f"https://api.github.com/repos/{User}/{Repository}/releases/latest"
-
 # GETリクエスト 送信
 with request.urlopen(URL) as response:
     data = json.load(response)
