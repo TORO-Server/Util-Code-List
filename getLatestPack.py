8c549cdce0b691f0484b25e0f5f74e732df88f15
malken21 <Malken_Marumasa@outlook.jp>
2023-10-29 02:19:13 +0900

    Update getLatestPack.py

--- getLatestPack.py
+++ getLatestPack.py
@@ -1,15 +1,19 @@
 from urllib import request
 import json
 
-# -----設定項目----- start
-User = "TORO-Server"
-Repository = "TORO-ResourcePack"
-# -----設定項目----- end
+
+def getLatest(User: str, Repository: str):
+    # URL
+    URL = f"https://api.github.com/repos/{User}/{Repository}/releases/latest"
+    # GETリクエスト 送信
+    with request.urlopen(URL) as response:
+        data = json.load(response)
+        return data["assets"][0]["browser_download_url"]
 
 
-# URL
-URL = f"https://api.github.com/repos/{User}/{Repository}/releases/latest"
-# GETリクエスト 送信
-with request.urlopen(URL) as response:
-    data = json.load(response)
-    print(data["assets"][0]["browser_download_url"])
+if __name__ == '__main__':
+    # -----設定項目----- start
+    User = "TORO-Server"
+    Repository = "TORO-ResourcePack"
+    # -----設定項目----- end
+    print(getLatest(User, Repository))
